4a18bb31e465bb54ac091b3f4ee1e0b146217fd1
RISHIT GHOSH <rishitghosh06@gmail.com>
2025-09-28 22:19:21 +0530

    Added CODE to auth package.

--- backend/src/main/java/com/campusconnect/auth/AuthDAO.java
+++ backend/src/main/java/com/campusconnect/auth/AuthDAO.java
@@ -1,7 +1,31 @@
 package com.campusconnect.auth;
 
+import com.campusconnect.utils.DBUtil;
+import java.sql.*;
+
 public class AuthDAO
 {
-    
-    
+    public User login(String email, String password) throws Exception
+    {
+        Connection conn = DBUtil.getConnection();
+        PreparedStatement ps = conn.prepareStatement(
+                "SELECT * FROM users WHERE email = ? AND password = ?"
+        );
+        ps.setString(1, email);
+        ps.setString(2, password);
+        ResultSet rs = ps.executeQuery();
+
+        User user = null;
+        if (rs.next())
+        {
+            user = new User();
+            user.setId(rs.getInt("id"));
+            user.setName(rs.getString("name"));
+            user.setEmail(rs.getString("email"));
+            user.setPassword(rs.getString("password"));
+        }
+
+        conn.close();
+        return user;
+    }
 }
--- backend/src/main/java/com/campusconnect/auth/AuthServlet.java
+++ backend/src/main/java/com/campusconnect/auth/AuthServlet.java
@@ -1,7 +1,34 @@
 package com.campusconnect.auth;
 
-public class AuthServlet
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.*;
+
+public class AuthServlet extends HttpServlet
 {
-    
-    
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+    {
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        try
+        {
+            User user = new AuthDAO().login(email, password);
+            if (user != null)
+            {
+                HttpSession session = req.getSession();
+                session.setAttribute("user", user);
+                res.sendRedirect("dashboard.jsp");
+            }
+            else
+            {
+                res.sendRedirect("login.jsp?error=invalid");
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            res.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
